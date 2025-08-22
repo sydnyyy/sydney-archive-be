1275f4db597fb4003168dae8355b884aa47d1589
sydnyyy <nayoungko28@gmail.com>
2025-08-22 13:33:43 +0900

    Initial commit

---
+++ src/main/java/com/wishlistbe/WishlistBeApplication.java
@@ -0,0 +1,13 @@
+package com.wishlistbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishlistBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishlistBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wishlistbe/WishlistBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wishlistbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
