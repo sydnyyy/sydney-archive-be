56f402120593f31a7be6ba0cbca144ff75c0f52b
sydnyyy <nayoungko28@gmail.com>
2025-09-13 21:11:20 +0900

    feat: Redis Stream 종료 명령 소비 및 웹소켓 세션 강제 종료 처리 추가
    - @Scheduled 로 terminate_session_stream 폴링
    - clientId 기반으로 관련된 WebSocket 세션 강제 종료
    - 마지막 처리한 Stream ID를 저장하여 중복 처리 방지

--- src/main/java/com/wishlist/global/config/websocket/manager/WebSocketSessionManager.java
+++ src/main/java/com/wishlist/global/config/websocket/manager/WebSocketSessionManager.java
@@ -2,11 +2,13 @@ package com.wishlist.global.config.websocket.manager;
 
 import com.wishlist.global.config.websocket.interceptor.WebSocketHandshakeInterceptor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketSession;
 
+import java.io.IOException;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.List;
@@ -16,6 +18,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class WebSocketSessionManager {
 
     private final StringRedisTemplate redisTemplate;
@@ -63,7 +66,28 @@ public class WebSocketSessionManager {
         );
     }
 
+    public void removeAllSessions(String clientId) {
+        Set<String> sessionIds = clientSessionIds.remove(clientId);
+        if (sessionIds == null || sessionIds.isEmpty()) return;
+
+        sessionIds.forEach(sessionId -> {
+            WebSocketSession webSocketSession = sessionMap.get(sessionId);
+            try {
+                log.info("[removeAllSessions] WebSocket session 강제 종료. clientId={}, sessionId={}", clientId, sessionId);
+                webSocketSession.close();
+                sessionMap.remove(sessionId);
+            } catch (IOException e) {
+                // TODO 웹소켓 강제 종료 재시도
+            }
+        });
+    }
+
     public void removeSession(WebSocketSession session) {
+        String sessionId = session.getId();
+        if (!sessionMap.containsKey(sessionId)) {
+            return;
+        }
+
         String clientId = session.getAttributes().get(WebSocketHandshakeInterceptor.CLIENT_ID_KEY).toString();
         String mainKey = WEBSOCKET_SESSION_MAIN_KEY_PREFIX + clientId;
         String terminateSignalKey = WEBSOCKET_SESSION_TERMINATE_SIGNAL_KEY_PREFIX + clientId;
