a0ff9aa1de85ec26eed3eb9b6b4c84dee8ed28f4
sydnyyy <nayoungko28@gmail.com>
2025-09-24 01:57:49 +0900

    refactor: 컬렉션 간 데이터 일관성을 위해 Striped Lock 패턴 적용
    - clientId 해시 기반 인덱스로 LockWithIndex 제공

--- src/main/java/com/wishlist/useractivity/manager/UserAccessManager.java
+++ src/main/java/com/wishlist/useractivity/manager/UserAccessManager.java
@@ -1,14 +1,27 @@
 package com.wishlist.useractivity.manager;
 
+import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Component
+@Slf4j
 public class UserAccessManager {
+
+    private static final int STRIPE_COUNT = 64;
+    private static final Lock[] locks = new Lock[STRIPE_COUNT];
+
+    static {
+        for (int i = 0; i < locks.length; i++) {
+            locks[i] = new ReentrantLock();
+        }
+    }
 
     private final Map<String, Long> lastAccessTimeMap = new ConcurrentHashMap<>();
     private final Set<AccessEntry> accessSortedSet = new ConcurrentSkipListSet<>();
@@ -44,15 +57,42 @@ public class UserAccessManager {
         }
     }
 
-    public void recordAccess(String clientId) {
-        Long previousTimestamp = lastAccessTimeMap.get(clientId);
-        if (previousTimestamp != null) {
-            accessSortedSet.remove(new AccessEntry(previousTimestamp, clientId));
+    private static class LockWithIndex {
+        final Lock lock;
+        final int index;
+
+        LockWithIndex(Lock lock, int index) {
+            this.lock = lock;
+            this.index = index;
         }
 
-        long now = System.currentTimeMillis();
-        lastAccessTimeMap.put(clientId, now);
-        accessSortedSet.add(new AccessEntry(now, clientId));
+        void lock() { lock.lock(); }
+        void unlock() { lock.unlock(); }
+    }
+
+    private LockWithIndex getLock(String clientId) {
+        int index = clientId.hashCode() & (STRIPE_COUNT - 1);
+        return new LockWithIndex(locks[index], index);
+    }
+
+    public void recordAccess(String clientId) {
+        LockWithIndex lock = getLock(clientId);
+        lock.lock();
+        try {
+            log.info("[recordAccess] clientId='{}' acquired lock index={}", clientId, lock.index);
+
+            Long previousTimestamp = lastAccessTimeMap.get(clientId);
+            if (previousTimestamp != null) {
+                accessSortedSet.remove(new AccessEntry(previousTimestamp, clientId));
+            }
+
+            long now = System.currentTimeMillis();
+            lastAccessTimeMap.put(clientId, now);
+            accessSortedSet.add(new AccessEntry(now, clientId));
+        } finally {
+            lock.unlock();
+            log.info("[recordAccess] clientId='{}' released lock index={}", clientId, lock.index);
+        }
     }
 
     public List<String> removeExpiredClientIds(long cutoff) {
