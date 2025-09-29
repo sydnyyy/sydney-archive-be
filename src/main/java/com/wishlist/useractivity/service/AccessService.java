2dd738d245e60e25f7f2d12ec3f608b701cee5e0
sydnyyy <nayoungko28@gmail.com>
2025-09-29 10:45:37 +0900

    chore: 접근 이벤트 DB 영속화 중단

--- src/main/java/com/wishlist/useractivity/service/AccessService.java
+++ src/main/java/com/wishlist/useractivity/service/AccessService.java
@@ -3,7 +3,6 @@ package com.wishlist.useractivity.service;
 import com.wishlist.global.websocket.manager.WebSocketSessionManager;
 import com.wishlist.useractivity.entity.AccessEvent;
 import com.wishlist.useractivity.manager.UserAccessManager;
-import com.wishlist.useractivity.repository.AccessRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +10,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AccessService {
 
-    private final AccessRepository accessRepository;
     private final WebSocketSessionManager webSocketSessionManager;
     private final UserAccessManager userAccessManager;
 
     public void recordAccess(AccessEvent accessEvent) {
-        accessRepository.save(accessEvent);
         webSocketSessionManager.updateTTL(accessEvent.clientId());
         userAccessManager.recordAccess(accessEvent.clientId());
     }
