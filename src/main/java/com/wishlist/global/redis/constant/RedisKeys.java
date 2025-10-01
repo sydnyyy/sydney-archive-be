e765fc1e35580dd1fb99e5a3bc212af1d374554d
sydnyyy <nayoungko28@gmail.com>
2025-10-01 14:22:17 +0900

    feat: 사용자 활동 브로드캐스트 기능 추가

---
+++ src/main/java/com/wishlist/global/redis/constant/RedisKeys.java
@@ -0,0 +1,6 @@
+package com.wishlist.global.redis.constant;
+
+public final class RedisKeys {
+
+    public static final String STREAM_KEY_USER_ACTIVITY = "BROADCAST:USER_ACTIVITY";
+}
--- src/main/java/com/wishlist/useractivity/service/AccessService.java
+++ src/main/java/com/wishlist/useractivity/service/AccessService.java
@@ -1,10 +1,15 @@
 package com.wishlist.useractivity.service;
 
+import com.wishlist.global.redis.constant.RedisKeys;
 import com.wishlist.global.websocket.manager.WebSocketSessionManager;
 import com.wishlist.useractivity.entity.AccessEvent;
 import com.wishlist.useractivity.manager.UserAccessManager;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -12,9 +17,17 @@ public class AccessService {
 
     private final WebSocketSessionManager webSocketSessionManager;
     private final UserAccessManager userAccessManager;
+    private final StringRedisTemplate redisTemplate;
 
     public void recordAccess(AccessEvent accessEvent) {
         webSocketSessionManager.updateTTL(accessEvent.clientId());
         userAccessManager.recordAccess(accessEvent.clientId());
+        broadcastUserActivity(accessEvent);
+    }
+
+    private void broadcastUserActivity(AccessEvent accessEvent) {
+        Map<String, String> message = new HashMap<>();
+        message.put("clientId", accessEvent.clientId());
+        redisTemplate.opsForStream().add(RedisKeys.STREAM_KEY_USER_ACTIVITY, message);
     }
 }
