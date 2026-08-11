67c4ac3a310fdef682e1ee8060b44b9c591ed980
sydnyyy <nayoungko28@gmail.com>
2026-08-11 15:45:42 +0900

    refactor: 웹 로그인 성공 리다이렉트 URL 리터럴 사용으로 가독성 개선

--- src/main/java/com/sydneyarchive/global/security/oauth2/handler/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/sydneyarchive/global/security/oauth2/handler/OAuth2AuthenticationSuccessHandler.java
@@ -81,9 +81,10 @@ public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccess
 
         if (loginSessionContext.platform() == Platform.WEB) {
             tokenService.issueRefreshTokenToCookie(
-                    userAuthContext.userId(), userAuthContext.role(), response);
+                    userAuthContext.userId(), userAuthContext.role(), response
+            );
 
-            String redirectUrl = frontendBaseUrl + "/login/success?platform=" + loginSessionContext.platform().toString().toLowerCase();
+            String redirectUrl = frontendBaseUrl + "/login/success?platform=web";
             response.sendRedirect(redirectUrl);
             return;
         }
